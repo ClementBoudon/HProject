6d1a0bd483cc0464254b5d85fe85142cd2c798c0
ClementBoudon <clement.boudon@gmail.com>
2015-01-27 15:54:56 +0100

    Ajout image Codeship Status

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
 /* Test Projet Codeship + Heroku Flow */
 echo 'Hello HProject<br>';
-echo 'Deployed with Codeship<br>';
+echo 'Deployed with <img src="https://www.codeship.io/projects/cdcd3fa0-351e-0132-92a7-5a0bd35c5084/status"><br>';
 echo 'Running on Heroku';
